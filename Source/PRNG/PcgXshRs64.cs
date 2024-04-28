c97bcda11113a879620c2644f9f3a17a33ee606f
Ricky Setiawan <29156554+Shiroechi@users.noreply.github.com>
2024-04-28 22:45:03 +0800

    Create PcgXshRs64.cs

---
+++ Source/PRNG/PcgXshRs64.cs
@@ -0,0 +1,152 @@
+﻿#if NET7_0_OR_GREATER
+using System;
+using System.Buffers.Binary;
+using System;
+using System.Security.Cryptography;
+
+namespace Litdex.Random.PRNG
+{
+	/// <summary>
+	///	A Permuted Congruential Generator (PCG) that is composed of 
+	///	a 128-bit Linear Congruential Generator (LCG) combined with 
+	///	the XSH-RS (xorshift; random shift) output transformation
+	///	to create 32-bit output.
+	/// </summary>
+	/// <remarks>
+	///	Source: https://www.pcg-random.org/
+	/// </remarks>
+	public class PcgXshRs64 : Random64
+	{
+		#region Member
+
+		protected UInt128 _State;
+		protected UInt128 _Increment = (UInt128)6364136223846793005 << 64 | 1442695040888963407;
+		protected UInt128 _Multiplier = (UInt128)2549297995355413924 << 64 | 4865540595714422341;
+
+		#endregion Member
+
+		#region Constructor & Destructor
+
+		/// <summary>
+		///	Create an instance of <see cref="PcgXshRs64"/> object.
+		/// </summary>
+		public PcgXshRs64() : this(UInt128.Zero, UInt128.Zero)
+		{
+
+		}
+
+		/// <summary>
+		///	Create an instance of <see cref="PcgXshRs64"/> object.
+		/// </summary>
+		/// <param name="seed">
+		///	RNG seed.
+		///	</param>
+		/// <param name="increment">
+		///	Increment step.
+		///	</param>
+		public PcgXshRs64(UInt128 seed, UInt128 increment)
+		{
+			this.SetSeed(seed, increment);
+		}
+
+		/// <summary>
+		///	Destructor.
+		/// </summary>
+		~PcgXshRs64()
+		{
+			this._State = 0;
+			this._Increment = 0;
+			this._Multiplier = 0;
+		}
+
+		#endregion Constructor & Destructor
+
+		#region Protected Method
+
+		/// <inheritdoc/>
+		protected override ulong Next()
+		{
+			var oldState = this._State;
+			
+			this._State = (oldState * _Multiplier) + (this._Increment);
+			
+			var rot = (int)(oldState >> 124);
+			return (ulong)(oldState ^ (oldState >> 43)) >> (45 + rot);
+		}
+
+		#endregion Protected Method
+
+		#region Public Method
+
+		/// <inheritdoc/>
+		public override string AlgorithmName()
+		{
+			return "PCG XSH-RS 64-bit";
+		}
+
+		/// <inheritdoc/>
+		public override void Reseed()
+		{
+			using (var random = RandomNumberGenerator.Create())
+			{
+				Span<byte> data = new byte[32];
+				random.GetNonZeroBytes(data);
+				this.SetSeed(
+					seed: BinaryPrimitives.ReadUInt128LittleEndian(data),
+					increment: BinaryPrimitives.ReadUInt128LittleEndian(data.Slice(16)));
+			}
+		}
+
+		/// <summary>
+		/// Advanced the state.
+		/// </summary>
+		/// <param name="delta">
+		/// Number of steps that the generator should advance forward.
+		/// </param>
+		public void Jump(int delta)
+		{
+			this._State = PcgStatic.PcgAdvanceLcg128(this._State, delta, this._Multiplier, this._Increment);
+		}
+
+		/// <summary>
+		///	Set RNG seed manually.
+		/// </summary>
+		/// <param name="seed">
+		///	RNG seed.
+		/// </param>
+		/// <param name="increment">
+		/// RNG increment.
+		/// </param>
+		public void SetSeed(UInt128 seed, UInt128 increment)
+		{
+			this._State = 0;
+			this._Increment = (increment << 1) | 1;
+			this._State = PcgStatic.PcgSetseq128StepR(this._State, this._Increment);
+			this._State += seed;
+			this._State = PcgStatic.PcgSetseq128StepR(this._State, this._Increment);
+		}
+
+		/// <summary>
+		///	Set RNG internal state manually.
+		/// </summary>
+		/// <param name="seed">
+		///	Number to generate the random numbers.
+		/// </param>
+		/// <exception cref="ArgumentNullException">
+		///	Array of seed is null or empty.
+		/// </exception>
+		public void SetSeed(params UInt128[] seed)
+		{
+			if (seed == null || seed.Length == 0)
+			{
+				throw new ArgumentNullException(nameof(seed), "Seed can't null or empty.");
+			}
+
+			this.SetSeed(seed[0], seed[1]);
+		}
+
+		#endregion Public Method
+	}
+}
+
+#endif
